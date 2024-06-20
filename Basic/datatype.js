622685e1f29144126f7dfda1b365fbc81ee5c200
mohit <patelmohit672@gmail.com>
2024-06-20 15:59:26 +0530

    first commit

---
+++ Basic/datatype.js
@@ -0,0 +1,8 @@
+
+let name = "Mohit"
+let age = 24
+
+console.log(typeof name);
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  // undefined
---
+++ Basic/variable.js
@@ -0,0 +1,23 @@
+const accountId = 14445;
+let accountEmail = "patelmohit672@gmail.com";
+var accountPassword = "mohit";
+
+accountCity = "Jaipur";
+
+// accountId = 2
+
+accountEmail = "mohit@1233";
+accountPassword = "rohiy";
+
+accountCity = "Delhi";
+let accountState;
+
+console.log(accountId);
+
+console.table(
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState
+);
