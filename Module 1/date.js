69a29661fac2b76f7095c916e04fd2f04f6b34e6
mohit <patelmohit672@gmail.com>
2024-06-21 14:06:14 +0530

    Module 1

---
+++ Module 1/date.js
@@ -0,0 +1,20 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleTimeString());
+
+// let createNewDate = new Date(2024, 5, 21)
+
+let createNewDate = new Date("2024-05-21")
+
+// console.log(createNewDate.toLocaleString());
+
+//  Timestamp +++++++++++++++++++++++++++++++++++++++
+
+let myTimeStamp = Date.now
+// console.log(myTimeStamp());
+// console.log(createNewDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
